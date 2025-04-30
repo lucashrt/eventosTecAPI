98550ac6e6fec70978af8c9c0a9d0a04deac00ed
lucashrt <hartmann.lucas@hotmail.com>
2025-04-29 23:36:44 -0300

    Service e Controller da criação do Event + Uploadimg para o Bucket

---
+++ src/main/java/com/eventostec/api/controller/EventController.java
@@ -0,0 +1,35 @@
+package com.eventostec.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.eventostec.api.domain.event.Event;
+import com.eventostec.api.domain.event.EventRequestDTO;
+import com.eventostec.api.service.EventService;
+
+@RestController
+@RequestMapping("/api/event")
+public class EventController {
+
+    @Autowired
+    private EventService eventService;
+
+    @PostMapping(consumes = "multipart/form-data")
+    public ResponseEntity<Event> createEvent(
+            @RequestParam("title") String title,
+            @RequestParam(value = "description", required = false) String description,
+            @RequestParam("date") Long date,
+            @RequestParam("city") String city,
+            @RequestParam("state") String state,
+            @RequestParam("remote") Boolean remote,
+            @RequestParam(value = "image", required = false) MultipartFile image,
+            @RequestParam("event_url") String event_url) {
+        EventRequestDTO body = new EventRequestDTO(title, description, date, city, state, remote, image, event_url);
+        return ResponseEntity.ok(this.eventService.createEvent(body));
+    }
+}
